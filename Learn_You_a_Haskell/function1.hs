e4e9eeb4714bca482e99de9c515d2da4cddabc19
John Wayne <ionut_popovici92@yahoo.com>
2017-05-15 23:58:10 +0300

    Learn You a Haskell (tutorial)

---
+++ Learn_You_a_Haskell/function1.hs
@@ -0,0 +1,3 @@
+-- a function that takes a number and multiplies it by two
+
+doubleMe x = x + x
---
+++ Learn_You_a_Haskell/function2.hs
@@ -0,0 +1,3 @@
+-- a function that takes two numbers and multiplies each by two and then adds them together
+
+doubleUs x y = x*2 + y*2
